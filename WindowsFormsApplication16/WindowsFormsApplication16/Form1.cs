51a32d4c50f7823674fe03559e48ec6a499fba97
vlada061098@gmail.com <vlada061098@gmail.com>
2020-05-10 17:13:24 +0300

    commit

---
+++ WindowsFormsApplication16/WindowsFormsApplication16/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication16
+{
+    public partial class Form1 : Form
+    {
+        private static Bitmap bmpScreenshot;
+        private static Graphics gfxScreenshot;
+        byte[] data;
+        int count_array=0;
+        bool flag;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            Thread thread = new Thread(Run); //запуск потока 
+            thread.Start();
+        }
+
+
+        public void Run() { 
+
+        var ip = IPAddress.Parse("127.0.0.1");
+        Socket sock1 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        sock1.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ExclusiveAddressUse, 1);
+            IPEndPoint iep1 = new IPEndPoint(ip, 9060); 
+            byte[] g = new byte[0];
+            sock1.SendTo(g, iep1);
+            flag = true;
+            do
+            {
+                Thread.Sleep(15);
+        int position = 0;
+
+        
+        //создание скриншота
+        bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+        gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+        gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+        //вызов функции сжатия скриншота
+        data = CompressBitmap(bmpScreenshot);
+
+        List<byte[]> send_file = new List<byte[]>(); //лист массивов байтов
+        
+                if (data.Length % 65002 != 0) //определение количества массивов байтов
+                {
+                    count_array = data.Length / 65002 + 1; //если остаток от деления не равен 0
+                }
+                else
+                    count_array = data.Length / 65002; //если остаток равен 0
+                while (position < data.Length) //пока position меньше количества байтов в data
+                {
+                    byte[] bytes;
+                    if ((data.Length - position) >= 65000)
+                        bytes = new byte[65002];
+                    else
+                    {
+                        bytes = new byte[data.Length - position + 2]; //объявление последнего массива байтов количеством оставшихся в потоке байт
+                    
+                    }
+                 
+                    Array.Copy(data, position, bytes, 0, bytes.Length-2); //заполнеям массив bytes с нужной позиции (кроме 2 последних байта)
+                    position += bytes.Length - 2; //увеличиваем позицию
+                    bytes[bytes.Length - 2] = (byte) (count_array); //заносим в 2 последних байта количество массивов байтов
+                    
+                    send_file.Add(bytes);//заносим в лист массив байтов
+                  
+                }
+             
+                
+                for (int i=0; i<count_array; i++)
+                {
+                    
+                    sock1.SendTo(send_file[i], iep1); //отправка листа массивов байтов
+                    Thread.Sleep(5); 
+
+                }
+
+            } while (flag);
+            sock1.Close(); //закрытие сокета
+    }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            flag = false;
+        }
+
+        private byte[] CompressBitmap(Bitmap bmp)
+        {
+            using (var zipped = new MemoryStream())
+            {
+                using (var gzip = new GZipStream(zipped, CompressionMode.Compress)) 
+                    bmp.Save(gzip, ImageFormat.Bmp);
+
+                return zipped.ToArray();
+                
+            }
+        }
+
+
+       
+    }
+}
---
+++ WindowsFormsApplication17/WindowsFormsApplication17/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication17
+{
+   
+    public partial class Form1 : Form
+    {
+        private static int portReceive = 9060;
+        private Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); //объявление сокета
+        private byte[] buffer = new byte[UInt16.MaxValue];
+        public List<byte[]> array = new List<byte[]>();
+        public int count_bytes = 0; //общее количество байтов
+        private static IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, portReceive);
+        private EndPoint endPoint = (EndPoint)ipEndPoint;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //связываем сокет с локальной конечной точкой
+            socket.Bind(endPoint);
+            //начинаем асинхронный прием данных
+            socket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref endPoint, new AsyncCallback(ReceiveCallback), socket);
+        }
+
+
+        public void ReceiveCallback(IAsyncResult ar)
+        {
+            
+                while (true)
+                {
+                
+                    int m = 0; //подсчет количества принятых массивов байтов
+                    short value;//количество массивов байтов, которые мы должны принять
+                    do
+                    {
+
+                        byte[] buffer_rez = new byte[65002];
+
+                        int n = socket.ReceiveFrom(buffer_rez, ref endPoint); //получаем датаграмму в буфер данных
+                   
+                        count_bytes += n - 2; 
+                        byte[] bytes_arr = new byte[n - 2];
+                        byte[] two_bytes = new byte[2];
+                        Array.Copy(buffer_rez, n - 2, two_bytes, 0, 2); //копирование из буфера в two_bytes 2 последних байта
+                        Array.Copy(buffer_rez, 0, bytes_arr, 0, bytes_arr.Length);//копирование в bytes_arr всех байтов кроме последних двух
+                        value = BitConverter.ToInt16(two_bytes, two_bytes.Length - 2); //получаем количество принимаемых байтов
+                        array.Add(bytes_arr); //заполняем лист массивов
+                        m++;
+
+                    } while (m != value);
+
+
+                    byte[] full_array = new byte[count_bytes];
+
+                    for (int i = 0; i < array.Count; i++)
+                    {
+                        Array.Copy(array[i], 0, full_array, i * 65000, array[i].Length); //полученные массивы байтов объединяем в один массив
+
+                    }
+
+                    byte[] result = Dec(full_array); //декомпрессия 
+
+                    MemoryStream memory_stream = new MemoryStream(result);
+                    System.Drawing.Bitmap bmp =
+                       (System.Drawing.Bitmap)System.Drawing.Bitmap.FromStream(memory_stream);
+                    pictureBox1.Image = bmp; //вставка изображения
+                //очищение для получения нового рисунка
+                    array.Clear();
+                    count_bytes = 0;
+
+                }
+          
+        }
+
+        static byte[] Dec(byte[] full_array)
+        {
+             
+                if (full_array == null)
+                    throw new ArgumentNullException("byteData", @"inputData must be non-null");
+
+                using (var compressedMs = new MemoryStream(full_array))
+                {
+                    using (var decompressedMs = new MemoryStream())
+                    {
+                        using (var gzs = new BufferedStream(new GZipStream(compressedMs, CompressionMode.Decompress), 4096))
+                        {
+                            gzs.CopyTo(decompressedMs);
+                        }
+                        return decompressedMs.ToArray();
+                    }
+                }
+            
+        }
+
+
+      
+
+        
+    }   
+}
